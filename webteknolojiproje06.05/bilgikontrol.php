0659332933609f26ba13d496b16190702a99feb1
Mehmet Okyay <mehmetokyay5406@gmail.com>
2025-05-06 21:53:26 +0300

    webproje 06/05/2025

---
+++ webteknolojiproje06.05/bilgikontrol.php
@@ -0,0 +1,12 @@
+<?php
+$kullaniciadi = $_POST['kullaniciadi'];
+$sifre = $_POST['sifre'];
+if (($kullaniciadi == "b241210007@sakarya.edu.tr") && ($sifre == "b241210007")) {
+    header("Refresh:2;url=anasayfa.html");
+    echo("Giriş Başarılı! Hoşgeldin B241210007!");
+}
+else {
+    header("Refresh:2;url=girissayfasi.html");
+    echo "Giriş Başarısız! Kullanıcı adı veya şifre hatalı. Giriş ekranına dönüyorsunuz...";
+}
+?>
