097238285b81f9aeace7ed956bc80608d6eedbaf
Mehmet Okyay <mehmetokyay5406@gmail.com>
2025-05-24 23:00:03 +0300

    webproje 24/05/2025

---
+++ webteknolojiproje24.05/iletisimbilgiler.php
@@ -0,0 +1,22 @@
+<?php
+$isim = $_POST["isim"];
+$soyisim = $_POST["soyisim"];
+$mail = $_POST["mail"];
+$adres = $_POST["adres"];
+$tarih = $_POST["tarih"];
+$telno = $_POST["telno"];
+$cinsiyet = $_POST["cinsiyet"];
+
+$eklenecekbilgiler = "İsim : " . $isim  . "\nSoyisim : " . $soyisim . "\nMail : " . $mail . "\nAdres : " . $adres . "\nTarih : " . $tarih . "\nTelefon Numarası : " . $telno . "\nCinsiyet : " . $cinsiyet . "\n"; 
+
+echo "İsim : " . $isim . "<br>";
+echo "Soyisim : " . $soyisim . "<br>";
+echo "Mail : " . $mail . "<br>";
+echo "Adres : " . $adres . "<br>";
+echo "Tarih : " . $tarih . "<br>";
+echo "Telefon Numarası : " . $telno . "<br>";
+echo "Cinsiyet : " . $cinsiyet . "<br>";
+
+file_put_contents("iletisimbilgileri.txt", $eklenecekbilgiler, FILE_APPEND | LOCK_EX);
+header("Refresh:2;url=iletisim.html");
+?>
